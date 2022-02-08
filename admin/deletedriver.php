21703d92b64cfb1a4118c27171553a7af7fabbea
archanasitu <archana.intelcorp@gmail.com>
2022-02-08 17:53:45 +0530

    add new template

--- admin/deletedriver.php
+++
@@ -1,8 +0,0 @@
-<?php
-      include('includes/config.php'); 
-        $id=$_GET['id'];
-        $delete = mysqli_query($conn, "DELETE FROM tbldriver WHERE id='$id'");
-        if ($delete) {
-          header("Location:manage-driver.php");
-        }   
-?>
