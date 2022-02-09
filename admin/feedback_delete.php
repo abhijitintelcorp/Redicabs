af7b91b2d52277465a892dfb374839e7e78d2be9
laxmipriyaswain <laxmipriya.intelcorp@gmail.com>
2022-02-09 16:20:41 +0530

    deleted

--- admin/feedback_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-        include "includes/config.php";
-        $id=$_GET['id'];
-        $delete = $conn->query("DELETE FROM add_feedback WHERE id=$id");
-            if ($delete) {
-                          header("Location: manage_feedback.php");
-                        }   
-?> 
