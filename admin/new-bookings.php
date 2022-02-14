02ecaf3ec764b6b8e245017516f356bd532a5bf6
pragyanbiswal <pragyanbiswal.intelcorp@gmail.com>
2022-02-14 17:19:49 +0530

    updated

--- admin/new-bookings.php
+++ admin/new-bookings.php
@@ -134,7 +134,7 @@ include("includes/config.php");
     <!-- AdminLTE App -->
     <script src="../../dist/js/adminlte.min.js"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="../../dist/js/demo.js"></script>
+  
     <!-- Page specific script -->
     <script>
     $(function() {
