ae0e4bd418893c62513b74adca457c9eff689092
archanasitu <archana.intelcorp@gmail.com>
2022-02-18 17:21:59 +0530

    updated

--- includes/header.php
+++ includes/header.php
@@ -49,34 +49,36 @@
             </div>
           </div>
         </div>
+        <div>
+          <div class="col-sm-4">
+            <div id="languages" class="resister-social">
 
-        <div class="col-sm-4">
-          <div id="languages" class="resister-social">
+              <div>
+                <!-- <a href="#">Login</a> -->
+                <button onclick="document.getElementById('id01').style.display='block'">Login</button>
+                <?php
+                include "login.php"
+                ?>
 
-            <div class="pull-right">
-              <!-- <a href="#">Login</a> -->
-              <button onclick="document.getElementById('id01').style.display='block'">Login</button>
-              <?php
-              include "login.php"
-              ?>
+                <div class="pull-right">
+                  <!-- <a href="#">Login</a> -->
+                  <button onclick="document.getElementById('id01').style.display='block'">Login</button>
+                  <?php
+                  include "login.php"
+                  ?>
 
-              <!-- <a href="#">Register</a> -->
-              <button onclick="document.getElementById('id02').style.display='block'">Register</button>
-              <?php
-              include "register.php"
-              ?>
+                </div>
 
-            </div>
-            <!--             <div class="social-icon">
+                <!--             <div class="social-icon">
               <p>paul@intelcorpsolutions.com</p>
             </div> -->
+              </div>
+            </div>
+
           </div>
         </div>
-
       </div>
     </div>
-  </div>
-</div>
-<div class="google-image">
-  <div id="map-canvas"></div>
-</div>
+    <div class="google-image">
+      <div id="map-canvas"></div>
+    </div>
