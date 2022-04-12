33b6dbce9de5b6207d39f4c8c4beb4cee5bcc0ce
rednuht93 <alok.intelcoro21@gmail.com>
2022-04-12 17:18:50 +0530

    Updated

--- my_booking.php
+++ my_booking.php
@@ -5,12 +5,42 @@ include("includes/connection.php");
 <?php
 //include("includes/header.php");
 $id = $_GET['id'];
+$_SESSION['id'] = $id;
 ?>
 <!doctype html>
 <html lang="en">
 <?php
 include("includes/login_header.php");
 ?>
+<style>
+    .profile_nav {
+        border-right: 1px solid #c5c5c5;
+        padding: 20px;
+        text-align: center;
+        background-color: #0e8fd5;
+        color: #ffffff;
+    }
+
+    .profile_nav ul {
+        padding: 0px;
+        margin: 0px;
+    }
+
+    .profile_nav ul li {
+        list-style: none;
+    }
+
+    .profile_nav ul li a {
+        color: #ffffff;
+        font-size: 15px;
+        font-weight: 900;
+    }
+
+    .profile_nav ul li.active a,
+    .profile_nav ul li a:hover {
+        color: #fa2837;
+    }
+</style>
 
 <body class="hold-transition sidebar-mini">
 
@@ -25,50 +55,82 @@ include("includes/login_header.php");
             $query = mysqli_query($conn, $sql);
             $results = mysqli_fetch_assoc($query);
             ?>
-            <div class="col-md-12 mybooking2">
-                <div>
-                    <h2 class="uppercase underline">My Bookings</h2>
-                    <hr width="150px" height="5px">
+            <div class="col-sm-12 mybooking2">
+                <div class="col-sm-12">
+                    <h2 class=" uppercase underline">My Bookings</h2>
+                    <hr width="100%" height="5px">
                 </div>
+
             </div>
             <div class="col-sm-8 mybooking">
 
+                <?php
+                $sql = "SELECT * from tblbooking where id='$id'";
+                $query = mysqli_query($conn, $sql);
+                while ($results = mysqli_fetch_assoc($query)) {
+                ?>
 
+                    <div>
 
-                <div>
+                        <div class="col-md-6 book1">
 
-                    <div class="col-md-6 book1">
+                            <img class="thumb" src="images/<?php echo $results['frontimage']; ?>" width="100%">
 
-                        <img class="thumb" src="images/<?php echo $results['frontimage']; ?>" width="100%">
+                        </div>
+                        <div class="col-md-6 book2">
 
+                            <input type="hidden" name="id" value="<?php echo $last_id; ?>">
+                            <p>
+                            <h3 style="color:red;">Welcome, <?php echo $results['UserName']; ?></h3>
+                            </p>
+                            <h5 class="uppercase underline">Booking Number :
+                                <span style="color:red;"># <?php echo $results['BookingNumber']; ?></span>
+                            </h5>
+                            <p>Vehicle Name: <span class="property_size"><?php echo $results['owner_vehicle_name']; ?></span>
+                            </p>
+
+                            <p> Vehicle Number: <span class="property_size"><?php echo $results['owner_vehicle_no']; ?></span>
+                            </p>
+
+                            <p>Pickup Place: <span class="property_size"><?php echo "<b>" . $results['pickup'] . "</b>"; ?></span>
+                            </p>
+                            <p>DropOff Place: <span class="property_size"><?php echo "<b>" . $results['dropoff'] . "</b>"; ?></b></span>
+                            </p>
+                            <?php
+                            if ($results['Status'] == 0) {
+                                $msg = "<b style='color:red;'>Not Confirmed</b>";
+                            } else if ($results['Status'] == 1) {
+                                $msg = "<b style='color:green;'>Confirmed</b>";
+                            } else if ($results['Status'] == 2) {
+                                $msg = "<b style='color:red;'>Cancelled</b>";
+                            } else if ($results['Status'] == 4) {
+                                $msg = "<b style='color:red;'>Delayed</b>";
+                            } else {
+                                $msg = "<b style='color:red;'>Journey Completed</b>";
+                            }
+                            ?>
+                            <p>Status: <span class="property_size"><?php echo $msg; ?></b></span>
+                            </p>
+                            <p class="price">Total Payment: <i class="fa fa-inr" style="font-size:18px"></i> <span class="property_size"><?php echo $results['Total']; ?></span>
+                            </p>
+                            <button class="payment">Make Payment</button>
+                        </div>
                     </div>
-                    <div class="col-md-6 book2">
-
-                        <input type="hidden" name="id" value="<?php echo $last_id; ?>">
-                        <p>
-                        <h3 style="color:red;">Welcome, <?php echo $results['UserName']; ?></h3>
-                        </p>
-                        <h5 class="uppercase underline">Booking Number :
-                            <span class="property_size"><?php echo $results['BookingNumber']; ?>
-                        </h5>
-                        <p>Vehicle Name: <span class="property_size"><?php echo $results['owner_vehicle_name']; ?></span>
-                        </p>
-
-                        <p> Vehicle Number: <span class="property_size"><?php echo $results['owner_vehicle_no']; ?></span>
-                        </p>
-
-                        <p>Pickup Place: <span class="property_size"><?php echo "<b>" . $results['pickup'] . "</b>"; ?></span>
-                        </p>
-                        <p>DropOff Place: <span class="property_size"><?php echo "<b>" . $results['dropoff'] . "</b>"; ?></b></span>
-                        </p>
-                        <p class="price">Total Payment: <i class="fa fa-inr" style="font-size:18px"></i> <span class="property_size"><?php echo $results['Total']; ?></span>
-                        </p>
-                        <button class="payment">Make Payment</button>
-                    </div>
-                </div>
+                <?php }  ?>
             </div>
 
             <div class="col-sm-4">
+                <div class="profile_nav">
+                    <ul>
+                        <li><a href="profile.php?id=<?php echo $id; ?>">Profile Settings</a></li>
+                        <hr>
+                        <li><a href="update-password.php?id=<?php echo $id; ?>">Update Password</a></li>
+                        <hr>
+                        <li><a href="my_booking.php?id=<?php echo $id; ?>">My Booking</a></li>
+                        <hr>
+                        <li><a href="logout.php">Sign Out</a></li>
+                    </ul>
+                </div>
                 <div class="card">
                     <h3>HELPING CENTER</h3>
                     <hr>
--- profile.php
+++ profile.php
@@ -2,95 +2,119 @@
 session_start();
 include("includes/connection.php");
 //error_reporting(0);
-$last_id = $_SESSION['last_id'];
-$sql = "SELECT * from tblbooking where id='$last_id'";
-$query = mysqli_query($conn, $sql);
-$results = mysqli_fetch_assoc($query);
-//$id = $_SESSION['last_id'];
+$id = $_GET['id'];
+$_SESSION['id'] = $id;
 if (isset($_POST['submit'])) {
-
     $UserName = htmlspecialchars($_POST['UserName']);
     $ContactNo = htmlspecialchars($_POST['ContactNo']);
     $address = htmlspecialchars($_POST['address']);
     $City = htmlspecialchars($_POST['City']);
     $EmailId = htmlspecialchars($_POST['EmailId']);
     $Password = htmlspecialchars($_POST['Password']);
-    $update_qry = "UPDATE 'tblbooking' SET UserName='$UserName',ContactNo ='$ContactNo',address='$address',City='$City',
-    EmailId='$EmailId',Password='$Password' WHERE id='$last_id'";
+    $update_qry = "UPDATE tblbooking SET UserName='$UserName',ContactNo ='$ContactNo',address='$address',City='$City',
+    EmailId='$EmailId',Password='$Password' WHERE id=$id";
     $query_run = mysqli_query($conn, $update_qry);
-    $last_id = mysqli_insert_id($conn);
-    $_SESSION['last_id'] = $last_id;
 
     if ($query_run) {
-        //echo "profile updated succesfully";
+        $msg2 = "<b style='color:green;'>Profile Updated Succesfully</b>";
     }
 }
 ?>
 
 <!doctype html>
 <html lang="en">
+<style>
+    .profile_nav {
+        border-right: 1px solid #c5c5c5;
+        padding: 20px;
+        text-align: center !important;
+        background-color: #0e8fd5;
+        color: #ffffff !important;
+    }
+
+    .profile_nav ul {
+        padding: 0px;
+        margin: 0px;
+    }
+
+    .profile_nav ul li {
+        list-style: none;
+    }
+
+    .profile_nav ul li a {
+        color: #ffffff !important;
+        font-size: 15px;
+        font-weight: 900;
+    }
+
+    .profile_nav ul li.active a,
+    .profile_nav ul li a:hover {
+        color: #fa2837;
+    }
+</style>
 
 <body class="hold-transition sidebar-mini">
     <?php
     include("includes/header.php");
     ?>
 
+
     <div class="container" id="booknow">
         <div class="row d-flex justify-content-center">
+            <div class="col-sm-12 mybooking2">
+                <div class="col-sm-12">
+                    <h2 class=" uppercase underline">My Profile</h2>
+                    <hr width="100%" height="5px">
+                </div>
 
+            </div>
 
-            <div class="col-md-9">
-                <form action="" class="form-box2" method="post" name="booking" id="booking">
-                    <input type="hidden" name="id" value="<?php echo $last_id; ?>">
+            <div class="col-sm-8">
+                <?php echo $msg2; ?>
+                <form action="" method="post" name="booking" id="booking">
                     <?php
-                    $sql = "SELECT * from tblbooking where id='$last_id'";
+                    $sql = "SELECT * from tblbooking where id='$id'";
                     $query = mysqli_query($conn, $sql);
                     $results = mysqli_fetch_assoc($query);
                     ?>
 
                     <div style="background-color: white">
-                        <h3 class="form-block-title"
-                            style="font-size: 30px; padding:5px; color: #f7eded;background-color: #1886bb;">
+                        <h3 class="form-block-title" style="font-size: 30px; padding:5px; color: #f7eded;background-color: #1886bb;">
                             Update Profile</h3>
                     </div>
 
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label>Name*</label>
-                            <input type="text" class="form-control24" name="UserName" id="UserName"
-                                value="<?php echo $results['UserName']; ?>">
+                            <input type="text" class="form-control24" name="UserName" id="UserName" value="<?php echo $results['UserName']; ?>">
 
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
-                            <label contact number *</label>
-                                <input type=" number" class="form-control24" name="ContactNo" id="ContactNo"
-                                    value="<?php echo $results['ContactNo']; ?>">
+                            <label>contact number *</label>
+                            <input type="number" class="form-control24" name="ContactNo" id="ContactNo" value="<?php echo $results['ContactNo']; ?>">
 
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
-                            <label Address *</label>
-                                <input type="text" class="form-control24" name="address" id="address"
-                                    value="<?php echo $results['address']; ?>">
+                            <label> Address *</label>
+                            <input type="text" class="form-control24" name="address" id="address" value="<?php echo $results['address']; ?>">
 
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label> City *</label>
-                            <input type="text" class="form-control24" name="City" id="City"
-                                value="<?php echo $results['City']; ?>">
+                            <input type="text" class="form-control24" name="City" id="City" value="<?php echo $results['City']; ?>">
 
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label> Email address *</label>
-                            <input type="text" class="form-control24" name="EmailId" id="EmailId"
-                                value="<?php echo $results['EmailId']; ?>">
+                            <input type="text" class="form-control24" name="EmailId" id="EmailId" value="<?php echo $results['EmailId']; ?>">
 
                         </div>
                     </div>
@@ -98,13 +122,26 @@ if (isset($_POST['submit'])) {
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label> password *</label>
-                            <input type="text" class="form-control24" name="Password" id="Password"
-                                value="<?php echo $results['Password']; ?>">
+                            <input type="text" class="form-control24" name="Password" id="Password" value="<?php echo $results['Password']; ?>">
 
                         </div>
                     </div>
-                    <center><button class="submit-btn btn" name="submit" type="submit">Save Changes</button></center>
+                    <center><button class="submit-btn btn" name="submit" type="submit">Update</button></center>
                 </form>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="profile_nav">
+                    <ul>
+                        <li><a href="profile.php?id=<?php echo $id; ?>">Profile Settings</a></li>
+                        <hr>
+                        <li><a href="update-password.php?id=<?php echo $id; ?>">Update Password</a></li>
+                        <hr>
+                        <li><a href="my_booking.php?id=<?php echo $id; ?>">My Booking</a></li>
+                        <hr>
+                        <li><a href="logout.php">Sign Out</a></li>
+                    </ul>
+                </div>
             </div>
 
         </div>
@@ -117,62 +154,62 @@ if (isset($_POST['submit'])) {
 
 </html>
 <script type="text/javascript">
-$(document).ready(function() {
-    $("#booking").validate({
-        rules: {
-            UserName: {
-                required: true,
-                minlength: 3,
-            },
-            ContactNo: {
-                required: true,
-                minlength: 10,
-            },
-            EmailId: {
-                required: true,
-                email: true,
-            },
-            Password: {
-                required: true,
-                minlength: 8,
-            },
-            address: {
-                required: true,
+    $(document).ready(function() {
+        $("#booking").validate({
+            rules: {
+                UserName: {
+                    required: true,
+                    minlength: 3,
+                },
+                ContactNo: {
+                    required: true,
+                    minlength: 10,
+                },
+                EmailId: {
+                    required: true,
+                    email: true,
+                },
+                Password: {
+                    required: true,
+                    minlength: 8,
+                },
+                address: {
+                    required: true,
 
-            },
-            City: {
-                required: true,
+                },
+                City: {
+                    required: true,
 
+                },
             },
-        },
-        messages: {
-            UserName: {
-                required: "<b style='color:red'>Please enter your Full Name</b>",
-                minlength: "<b style='color:red'>Full Name should be at least 3 characters</b>",
+            messages: {
+                UserName: {
+                    required: "<b style='color:red'>Please enter your Full Name</b>",
+                    minlength: "<b style='color:red'>Full Name should be at least 3 characters</b>",
+                },
+                EmailId: {
+                    required: "<b style='color:red'>Please enter Email Id</b>",
+                    email: "<b style='color:red'>The email should be in the format: abc@domain.tld</b>",
+                },
+                ContactNo: {
+                    required: "<b style='color:red'>Please enter your Mobile Number</b>",
+                    number: "<b style='color:red'>Please Enter numerical values Only</b>",
+                },
+                Password: {
+                    required: "<b style='color:red'>Please enter your Password</b>",
+                    minlength: "<b style='color:red'>Password should be at least 8 characters</b>",
+                },
+                address: {
+                    required: "<b style='color:red'>Please enter your address</b>",
+                    //minlength: "<b style='color:red'>Full Name should be at least 3 characters</b>",
+                },
+                City: {
+                    required: "<b style='color:red'>Please enter your city</b>",
+                }
             },
-            EmailId: {
-                required: "<b style='color:red'>Please enter Email Id</b>",
-                email: "<b style='color:red'>The email should be in the format: abc@domain.tld</b>",
+            submitHandler: function(form) {
+                form.submit();
             },
-            ContactNo: {
-                required: "<b style='color:red'>Please enter your Mobile Number</b>",
-                number: "<b style='color:red'>Please Enter numerical values Only</b>",
-            },
-            Password: {
-                required: "<b style='color:red'>Please enter your Password</b>",
-                minlength: "<b style='color:red'>Password should be at least 8 characters</b>",
-            },
-            address: {
-                required: "<b style='color:red'>Please enter your address</b>",
-                //minlength: "<b style='color:red'>Full Name should be at least 3 characters</b>",
-            },
-            City: {
-                required: "<b style='color:red'>Please enter your city</b>",
-            }
-        },
-        submitHandler: function(form) {
-            form.submit();
-        },
+        });
     });
-});
 </script>
